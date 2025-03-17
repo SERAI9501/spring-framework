979dbfe641645898544843d9a83609e6a41ba399
sist102 <sist102@DESKTOP-06VP6JK>
2025-03-18 08:55:08 +0900

    2025-03-18

--- SpringDIProject_1/src/main/java/com/sist/main/MainClass.java
+++ SpringDIProject_1/src/main/java/com/sist/main/MainClass.java
@@ -29,13 +29,23 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ApplicationContext app=
-				new ClassPathXmlApplicationContext("app1.xml");
-		//Sawon sa=(Sawon)app.getBean("sa1");
-		//sa.print();
-		
-		Sawon sa2=(Sawon)app.getBean("sa3");
-		sa2.print();
+        ApplicationContext app=
+        	 new ClassPathXmlApplicationContext("app1.xml");
+        Sawon sa=(Sawon)app.getBean("sa1");
+        sa.print();
+        
+        Sawon sa1=(Sawon)app.getBean("sa2");
+        sa1.print();
+        
+        Sawon sa2=(Sawon)app.getBean("sa3");
+        sa2.print();
+        
+        Sawon sa3=(Sawon)app.getBean("sa4");
+        sa3.print();
+        
+        Sawon sa4=(Sawon)app.getBean("sa5");
+        sa4.print();
+        
 	}
 
 }
